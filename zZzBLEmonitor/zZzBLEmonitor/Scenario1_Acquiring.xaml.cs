9e53cab39fc5f546e3db05f50b0b5cc72cd2b6ec
Unknown <zeky.os@gmail.com>
2017-09-29 16:14:31 -0700

    UART capabilities added
    -Added all the necessary code to communicate using UART to the MCU, however, it won't recognize the device giving a null value when loading the serial port.

--- zZzBLEmonitor/zZzBLEmonitor/Scenario1_Acquiring.xaml.cs
+++ zZzBLEmonitor/zZzBLEmonitor/Scenario1_Acquiring.xaml.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.ComponentModel;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Foundation;
@@ -66,9 +67,21 @@ namespace zZzBLEmonitor
         bool busy = false;//flag for when the writing function is busy
         Int32 counter = 0;
         Stopwatch stopwatch = new Stopwatch();
+
+        /*>>> UART Communication*/
+        static string imuIdString = "USB#VID_0451&PID_BEF3&MI_00#6&9aafbc6&0&0000#";
+        private SerialDevice imuSerialPort = null;
+        DataWriter imuDataWriteObject = null;
+        public string rawData = "";
+        public string imuData = "";
+        private ObservableCollection<DeviceInformation> listOfSerialDevices;
+        //private CancellationTokenSource ReadCancellationTokenSource;
+
         public Scenario1_Acquiring()
         {
             InitializeComponent();
+            listOfSerialDevices = new ObservableCollection<DeviceInformation>();
+            ListAvailablePorts();
         }
 
         ~Scenario1_Acquiring() { }
@@ -93,6 +106,77 @@ namespace zZzBLEmonitor
                 connectButton.IsEnabled = false;
             }
         }
+        /// <summary>
+        /// ListAvailablePorts
+        /// - Use SerialDevice.GetDeviceSelector to enumerate all serial devices
+        /// - Attaches the DeviceInformation to the ListBox source so that DeviceIds are displayed
+        /// </summary>
+        private async void ListAvailablePorts()
+        {
+            try
+            {
+                string aqs = SerialDevice.GetDeviceSelector();
+                var dis = await DeviceInformation.FindAllAsync(aqs);
+
+                foreach(var device in dis)
+                {
+                    if (device.Id.Contains(imuIdString))
+                    {
+                        listOfSerialDevices.Add(device);
+                        imuSerialPort = await SerialDevice.FromIdAsync(device.Id);
+                    }
+                }
+
+                if(imuSerialPort != null)
+                {
+                    imuDataWriteObject = new DataWriter(imuSerialPort.OutputStream);
+                    // Configure serial settings
+                    imuSerialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                    imuSerialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                    imuSerialPort.BaudRate = 9600;
+                    imuSerialPort.Parity = SerialParity.None;
+                    imuSerialPort.StopBits = SerialStopBitCount.One;
+                    imuSerialPort.DataBits = 8;
+                    imuSerialPort.Handshake = SerialHandshake.None;
+                }
+                else
+                {
+                    rootPage.notifyFlyout("null Serial Device", connectButton);
+                }
+            }
+            catch (Exception ex)
+            {
+                rootPage.notifyFlyout(ex.Message, connectButton);
+            }
+        }
+        /// <summary>
+        /// WriteAsync: Task that asynchronously writes data from the input text box 'sendText' to the OutputStream 
+        /// </summary>
+        /// <returns></returns>
+        private async Task WriteAsync(DataWriter dataWriteObject, byte[] command)
+        {
+            Task<UInt32> storeAsyncTask;
+
+            if (command.Length != 0)
+            {
+                // Load the text from the sendText input text box to the dataWriter object
+                //dataWriteObject.WriteString(command);
+                dataWriteObject.WriteBytes(command);
+
+                // Launch an async task to complete the write operation
+                storeAsyncTask = dataWriteObject.StoreAsync().AsTask();
+
+                UInt32 bytesWritten = await storeAsyncTask;
+                if (bytesWritten > 0)
+                {
+                    rootPage.notifyFlyout("Bytes written successfully!", connectButton);
+                }
+            }
+            else
+            {
+                rootPage.notifyFlyout("ERROR writting.", connectButton);
+            }
+        }
         // -->> Search BLE Sensor: Click Event <<--
         //_________________________________________
         private async void searchDeviceButton_Click(object sender, RoutedEventArgs e)
@@ -106,7 +190,7 @@ namespace zZzBLEmonitor
         private async void connectButton_Click(object sender, RoutedEventArgs e)
         {
             connectButton.IsEnabled = false;
-            graph.Initialize(graphStackPanel,1);
+            graph.Initialize(graphStackPanel, 1);
             graph.Background(Colors.WhiteSmoke);
             graph.AddPlot(Colors.Red);
             graph.AddPlot(Colors.Blue);
@@ -115,7 +199,7 @@ namespace zZzBLEmonitor
             if (IsValueChangedHandlerRegistered)
             {// Reconnecting
                 acquireButton.IsEnabled = false;
-                foreach(GattCharacteristic characteristic in selectedCharacteristic)
+                foreach (GattCharacteristic characteristic in selectedCharacteristic)
                 {
                     characteristic.ValueChanged -= Characteristic_ValueChanged;
                 }
@@ -158,8 +242,9 @@ namespace zZzBLEmonitor
                     {
                         selectedService = servicesResult.Services.Single();
                         Debug.WriteLine($"Service found: {selectedService.Uuid}");
-                        try {
-                        
+                        try
+                        {
+
                             // Gets the Breathing characteristic
                             characResult = await selectedService.GetCharacteristicsForUuidAsync(
                                             BrService.BREATHING_UUID, BluetoothCacheMode.Uncached);
@@ -173,7 +258,7 @@ namespace zZzBLEmonitor
                         { // SUCCESS!!!
                             int number = characResult.Characteristics.Count();
                             rootPage.notifyFlyout(number.ToString() + " characteristics found", connectButton);
-                            foreach(GattCharacteristic characteristic in characResult.Characteristics)
+                            foreach (GattCharacteristic characteristic in characResult.Characteristics)
                             {
                                 selectedCharacteristic.Add(characteristic);
                             }
@@ -185,6 +270,12 @@ namespace zZzBLEmonitor
                             rootPage.fileName = "data-" + rootPage.selectedDeviceName + DateTime.Now.ToString("_yyyy-dd-MM_HHmmss") + ".zZz";
                             rootPage.dataFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(rootPage.folderName, CreationCollisionOption.OpenIfExists);
                             rootPage.dataFile = await rootPage.dataFolder.CreateFileAsync(rootPage.fileName, CreationCollisionOption.ReplaceExisting);
+
+                            var uri = new System.Uri("ms-appx:///imu_RightSide_n63.txt");
+                            var file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+                            rawData = await FileIO.ReadTextAsync(file);//Reads file
+                            int index = rawData.IndexOf("0D0A00");//Looks for the first end of transmission characters
+                            rawData = rawData.Substring(index);//Eliminates everything before first en of transmission
                         }
                         else
                         {
@@ -225,7 +316,7 @@ namespace zZzBLEmonitor
                     await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
                         GattClientCharacteristicConfigurationDescriptorValue.Notify);
                     if (result == GattCommunicationStatus.Success)
-                    {   
+                    {
                         characteristic.ValueChanged += Characteristic_ValueChanged;
                         OnNewDataEnqueued += new ElementEnqueued(OnNewDataEnqueuedFxn);
                         OnNewPointsAdded += new PointsAdded(OnNewPointsAddedFxn);
@@ -233,6 +324,16 @@ namespace zZzBLEmonitor
                         acquireButton.Label = "Stop";
                         acquireButton.Icon = new SymbolIcon(Symbol.Stop);
                         stopwatch.Start();
+
+                        string writeData = rawData.Substring(6, 36);//Extracts first set of measurements
+                        rawData = rawData.Substring(42);//Eliminates read data from rawData
+                        byte[] dataByte = new byte[writeData.Length / 2];//Converts to bytes
+                        for (int i = 0; i < writeData.Length; i += 2)
+                        {
+                            dataByte[i / 2] = Convert.ToByte(writeData.Substring(i, 2), 16);
+                        }
+                        await WriteAsync(imuDataWriteObject, dataByte);
+
                     }
                     else
                     {
@@ -256,29 +357,6 @@ namespace zZzBLEmonitor
                 acquireButton.Icon = new FontIcon { Glyph = "\uE9D9" };
             }
             acquireButton.IsEnabled = true;
-
-            #region //Code for direct reading
-            /*GattReadResult result = null;
-            try
-            {
-                result = await selectedCharacteristic.ReadValueAsync(BluetoothCacheMode.Uncached);
-            }
-            catch (Exception ex)
-            {
-                notifyFlyout("Acquiring failed! " + ex.Message);
-            }
-            
-            if (result.Status == GattCommunicationStatus.Success)
-            {
-                byte[] data;
-                CryptographicBuffer.CopyToByteArray(result.Value, out data);
-                dataTextBlock.Text = BitConverter.ToInt32(data, 0).ToString();
-            }
-            else
-            {
-                notifyFlyout("Acquiring failed");
-            }*/
-            #endregion
         }
         //New data is available at the sensor.
         private async void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
@@ -312,7 +390,26 @@ namespace zZzBLEmonitor
                         counter = 0;
                     }
                 }
-
+                if ((rawData.Length >= 42))// && (timesTicked <= 60))//Is there enough data for next sample?
+                {
+                    string writeData = rawData.Substring(6, 36);//Extracts first set of measurements
+                    rawData = rawData.Substring(42);//Eliminates read data from rawData
+                    byte[] dataByte = new byte[writeData.Length / 2];//Converts to bytes
+                    for (int i = 0; i < writeData.Length; i += 2)
+                    {
+                        dataByte[i / 2] = Convert.ToByte(writeData.Substring(i, 2), 16);
+                    }
+                    await WriteAsync(imuDataWriteObject, dataByte);
+                }
+                else
+                {
+                    if (imuSerialPort != null)
+                    {
+                        imuSerialPort.Dispose();
+                    }
+                    imuSerialPort = null;
+                    listOfSerialDevices.Clear();
+                }
                 // Triggers queue event
                 if (OnNewDataEnqueued != null)
                 { OnNewDataEnqueued(this, EventArgs.Empty); }
@@ -335,7 +432,7 @@ namespace zZzBLEmonitor
             {//Writes to file
                 busy = true;//Sets busy flag
                 thetaClass dequeued = null;
-                while(dataQueue.Count > 0)
+                while (dataQueue.Count > 0)
                 {
                     if (dataQueue.TryDequeue(out dequeued))//Dqueues new data
                     { await FileIO.AppendTextAsync(rootPage.dataFile, dequeued.StringTheta); }
@@ -356,7 +453,7 @@ namespace zZzBLEmonitor
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
                 () =>
                 {
-                    counter +=6;
+                    counter += 6;
                     counterTextBlock.Text = counter.ToString();
                     thetaClass dequeued = null;
                     while (dataQueue.Count > 0)
@@ -371,158 +468,5 @@ namespace zZzBLEmonitor
                 });
 
         }
-
-        /*private async void WriteToFile(string data)
-        {
-            lock (fileLock)
-            {
-                FileIO.AppendText(rootPage.dataFile, data.ToString() + "," + DateTime.Now.ToString("HH:mm:ss.fff") + "\r\n");
-            }
-            //await FileIO.AppendTextAsync(rootPage.dataFile, data + "\r\n");
-            
-        }*/
-
-        //private async Task
-
-        //_______________________________________________
-        //-----------------------------------------------
-        //  BLE Attributes - Services and Characteristics
-        //_______________________________________________
-
-        /*private void ClearBLEDevice()
-        {
-            sensorTagBLE?.Dispose();
-            sensorTagBLE = null;
-        }
-
-        private async void servicesWatcher()
-        {// Here we start looking at the device's attributes
-
-
-            ClearBLEDevice();
-            ServiceCollection.Clear();
-
-            try
-            {
-                // BT_Code: BluetoothLEDevice.FromIdAsync must be called from a UI thread because it may prompt for consent.
-                sensorTagBLE = await BluetoothLEDevice.FromIdAsync(rootPage.selectedDeviceId);
-            }
-            catch (Exception ex) when ((uint)ex.HResult == 0x800710df)
-            {
-                // ERROR_DEVICE_NOT_AVAILABLE because the Bluetooth radio is not on.
-            }
-
-            if (sensorTagBLE != null)
-            {
-                // BT_Code: GattServices returns a list of all the supported services of the device.
-                // If the services supported by the device are expected to change
-                // during BT usage, subscribe to the GattServicesChanged event.
-                foreach (var service in sensorTagBLE.GattServices)
-                {
-                    ServiceCollection.Add(new BLEAttributeDisplay(service));
-                }
-
-
-                //connectButton.Visibility = Visibility.Collapsed;
-                //servicesListView.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                //ClearBluetoothLEDevice();
-                Debug.WriteLine("!!! Connection failed !!!");
-            }
-
-        }
-
-        private async void servicesComboBox_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            var attributeInfoDisp = (BLEAttributeDisplay)servicesComboBox.SelectedItem;
-
-            CharacteristicCollection.Clear();
-            IReadOnlyCollection<GattCharacteristic> characteristics = null;
-            try
-            {
-                // Get all the child characteristics of a service
-                characteristics = attributeInfoDisp.service.GetAllCharacteristics();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Restricted service. Can't read characteristics: " + ex.Message);
-                characteristics = new List<GattCharacteristic>();
-            }
-
-            foreach (GattCharacteristic c in characteristics)
-            {
-                CharacteristicCollection.Add(new BLEAttributeDisplay(c));
-            }
-        }
-
-        private DeviceInformationDisplay FindDeviceDisplay(string id)
-        {// Searchs for a device in the collection
-            foreach (DeviceInformationDisplay deviceDisplay in ResultCollection)
-            {
-                if (deviceDisplay.Id == id)
-                {
-                    return deviceDisplay;
-                }
-            }
-            return null;
-        }
-
-        /*Boolean ledState = false;
-
-        private async void ledButton_Click()
-        {
-            var attributeInfoDisp = (BLEAttributeDisplay)characteristicsListView.SelectedItem;
-            selectedCharacteristic = attributeInfoDisp.characteristic;
-            var writer = new DataWriter();
-            if (ledState)
-            {// It's ON. Turn it OFF
-                writer.WriteByte((Byte)0x00);
-                ledState = false;
-            }
-            else
-            {// It's OFF. Turn it ON
-                writer.WriteByte((Byte)0x01);
-                ledState = true;
-            }
-            await selectedCharacteristic.WriteValueAsync(writer.DetachBuffer());
-        }*/
     }
-
-    /*public class DataStorage : INotifyPropertyChanged
-    {
-        private AcquiredData dataAcquired;
-
-        public DataStorage(AcquiredData dataAcquiredIn)
-        {
-            dataAcquired = dataAcquiredIn;
-        }
-
-        public DateTime timeStamp
-        {
-            get
-            {
-                return dataAcquired.TimeStamp;
-            }
-        }
-
-        public Int16 data
-        {
-            get
-            {
-                return dataAcquired.Data;
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(name));
-            }
-        }
-    }*/
 }
